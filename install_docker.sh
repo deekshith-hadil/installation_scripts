5e62f5f066aed08ee6eaa20de5a4e6935860d2a9
root <root@postfix01.dankterminal.com>
2020-06-16 10:17:37 -0400

    create script for docker installation on redhat

--- install_docker.sh
+++ install_docker.sh
@@ -1,1 +1,25 @@
-script one
+#!/bin/bash
+#function for redhat family
+redhat_install()
+{
+    yum remove -y docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-engine
+    yum install -y yum-utils
+    yum-config-manager \
+        --add-repo \
+        https://download.docker.com/linux/centos/docker-ce.repo
+    yum install -y docker-ce docker-ce-cli containerd.io
+    systemctl enable docker
+    systemctl start docker
+}
+
+if [ -f /etc/redhat-release ]
+then
+    redhat_install
+fi
